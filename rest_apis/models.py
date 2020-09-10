0f2ec75346d29be48d09d7ef0ff02cdee159ff1d
kananjoshi <joshikanan32@gmail.com>
2020-09-10 05:58:36 +0530

    [Changes]

--- rest_apis/models.py
+++ rest_apis/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(_("Product"), max_length=50, blank=True, null=True)
     price = models.IntegerField(_("Price"))
     category = models.ForeignKey(Category, related_name=_("category"), on_delete=models.DO_NOTHING)
-    image = models.ImageField(_("Product Image"), upload_to='/media/proucts/')
+    image = models.ImageField(_("Product Image"), upload_to='media/proucts/')
 
     class Meta:
         verbose_name = _("Product")
